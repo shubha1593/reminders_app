d3cd9b22b6c53d704838062501160a031b12db02
Shubha Gupta <shubha1593@gmail.com>
2019-04-01 05:39:41 +0530

    Create and migrate reminder model.

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Reminder(models.Model):
+	title = models.CharField(max_length=500)
+	date = models.DateField()
+	time = models.TimeField()
+	note = models.TextField()
 
+	def __str__(self):
+		return self.title
 
