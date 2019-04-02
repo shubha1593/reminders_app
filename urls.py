324994e40cb62371068e28c8d25ea0adfb779bd1
Shubha Gupta <shubha1593@gmail.com>
2019-04-02 11:49:28 +0530

    Add templates

--- urls.py
+++ urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
+	# /reminders_app
 	path('', views.index, name='index'),
+	# /reminders_app/7/
+	path('<int:reminder_id>/', views.detail, name='detail'),
+	# /reminders_app/add/
+	path('add/', views.add, name='add'),
 ]
--- views.py
+++ views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+#from django.template import loader
+
+from .models import Reminder
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello World!")
+	latest_reminder_list = Reminder.objects.order_by("-date")[:5]
+	#template = loader.get_template("reminders_app/index.html")
+	context = {
+		'latest_reminder_list': latest_reminder_list,
+	}
+	return render(request,'reminders_app/index.html' ,context)
+
+def detail(request, reminder_id):
+	reminder = get_object_or_404(Reminder, pk=reminder_id)
+	return render(request, 'reminders_app/detail.html', {'reminder': reminder})
+
+def add(request):
+	return HttpResponse("You're adding a reminder.")
