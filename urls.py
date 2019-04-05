3e12bc5c4c20d658ef8872ec508339971cffa2c4
Shubha Gupta <shubha1593@gmail.com>
2019-04-05 09:28:26 +0530

    Use generic views

--- urls.py
+++ urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'reminders_app'
+
 urlpatterns = [
 	# /reminders_app
-	path('', views.index, name='index'),
+	path('', views.IndexView.as_view(), name='index'),
 	# /reminders_app/7/
-	path('<int:reminder_id>/', views.detail, name='detail'),
+	path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 	# /reminders_app/add/
 	path('add/', views.add, name='add'),
+	# /reminders_app/insert/
+	path('insert/', views.insert, name='insert'),
 ]
--- views.py
+++ views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
 #from django.template import loader
 
 from .models import Reminder
 
-# Create your views here.
-def index(request):
-	latest_reminder_list = Reminder.objects.order_by("-date")[:5]
-	#template = loader.get_template("reminders_app/index.html")
-	context = {
-		'latest_reminder_list': latest_reminder_list,
-	}
-	return render(request,'reminders_app/index.html' ,context)
+class IndexView(generic.ListView):
+	template_name = 'reminders_app/index.html'
+	context_object_name = 'latest_reminder_list'
 
+	def get_queryset(self):
+		""" Return the last five reminders. """
+		return Reminder.objects.order_by("-date")[:5]
+
+class DetailView(generic.DetailView):
+	model = Reminder
+	template_name = 'reminders_app/detail.html'
+		
 def detail(request, reminder_id):
 	reminder = get_object_or_404(Reminder, pk=reminder_id)
 	return render(request, 'reminders_app/detail.html', {'reminder': reminder})
 
 def add(request):
-	return HttpResponse("You're adding a reminder.")
+	return render(request, 'reminders_app/add.html')
+
+def insert(request):
+	data_dict = request.POST
+	context = {}
+	try:
+		reminder = Reminder.objects.create(title=data_dict['title'], date=data_dict['date'], time=data_dict['time'], note=data_dict['note'])
+		context['message'] = "Reminder added successfully!"
+		context['outcome'] = "green"
+	except(KeyError, Reminder.DoesNotExist):
+		context['message'] = "Error adding reminder. Make sure you filled in all the fields!"
+		context['outcome'] = "red"
+	return HttpResponseRedirect(reverse('reminders_app:detail', args=(reminder.id,)))
+
