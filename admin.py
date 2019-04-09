3c16ab34e22aca756f867fd1f14fb0e049823660
Shubha Gupta <shubha1593@gmail.com>
2019-04-09 18:59:01 +0530

    Add stylesheet.

--- admin.py
+++ admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Reminder
 
+class ReminderAdmin(admin.ModelAdmin):
+	fieldsets = [
+	(None, {'fields': ['title']}),
+	('When to remind', {'fields': ['date', 'time']}),
+	('Details', {'fields': ['note']}),
+	]
+	list_display = ('title', 'date', 'time')
+	list_filter = ['date']
+
 # Register your models here.
-admin.site.register(Reminder)
+admin.site.register(Reminder, ReminderAdmin)
